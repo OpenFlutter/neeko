f016f74c6562e7c5a7fdbcddc819a9a384b644a3
JarvanMo <jarvan.mo@gmail.com>
2019-09-01 10:16:31 +0800

    update demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -57,7 +57,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
 //    SystemChrome.setPreferredOrientations(orientations)
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.restoreSystemUIOverlays();
+    super.dispose();
   }
 
   exit() {
@@ -111,14 +118,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: NeekoPlayerWidget(
-          videoPlayerController: beeController,
-        ),
+      body: NeekoPlayerWidget(
+        videoPlayerController: beeController,
+      ),
     );
   }
 }
